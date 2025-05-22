9f268f9dfd133392bf7f3e90bc25ffab1b750641
Brave <aimebrave34@gmail.com>
2025-05-22 20:02:43 +0200

    Initial commit of my Code Wars Challenges

---
+++ 7 Kyu/mumbling.js
@@ -0,0 +1,15 @@
+function accum(s) {
+  const result = []; 
+  for (let i = 0; i < s.length; i++) {
+    const char = s[i]; 
+    const capitalizedChar = char.toUpperCase();
+    const lowerChar = char.toLowerCase();
+    const repeatedLower = lowerChar.repeat(i);
+    const part = capitalizedChar + repeatedLower;
+
+    result.push(part);
+  }
+
+  return result.join('-');
+}
+
---
+++ 8 Kyu/keepHydrated.js
@@ -0,0 +1,4 @@
+function litres(time) {
+  let amount = time * 0.5;
+  return  Math.floor(amount);
+}
