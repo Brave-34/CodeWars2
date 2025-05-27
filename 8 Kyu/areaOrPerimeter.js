b8f1acca361d6347c7935a463b3e846be1d8b647
Brave <aimebrave34@gmail.com>
2025-05-27 10:12:51 +0200

    Solved

--- 8 Kyu/areaOrPerimeter.js
+++ 8 Kyu/areaOrPerimeter.js
@@ -1,20 +1,9 @@
 <<<<<<< HEAD:8 Kyu/areaOrPerimeter.js
 const areaOrPerimeter = function(l , w) {
-  // Return your answer
   if (l === w){
     return l * w;
   }
   else{
     return 2 * (l + w);
   }
-=======
-const areaOrPerimeter = function(l , w) {
-  // Return your answer
-  if (l === w){
-    return l * w;
-  }
-  else{
-    return 2 * (l + w);
-  }
->>>>>>> d1821fa851af0a539adb521bad030b20fbbac4ef:8 Kyu/Sum of Positive/areaOrPerimeter.js
-};
+}
