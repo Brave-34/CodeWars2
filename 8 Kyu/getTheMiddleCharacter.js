de8d8ac6f340ed9ea3b9a9d87aec0a13cf7a47da
Brave <aimebrave34@gmail.com>
2025-05-29 16:47:17 +0200

    Solved

---
+++ 8 Kyu/getTheMiddleCharacter.js
@@ -0,0 +1,16 @@
+function getMiddle(s) {
+ let length = s.length;
+ let char1, char2;
+
+for (let i = 0; i < length; i++){
+     if(length % 2 === 0){
+     char1 = s[length / 2];
+     char2 = s[(length / 2) - 1];
+     return char2 + char1;
+ }
+ else{
+     char1 =s[Math.floor(length / 2)];
+     return char1;
+ }
+}
+}
