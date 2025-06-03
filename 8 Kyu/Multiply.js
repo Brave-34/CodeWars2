2072b8abf8873f3e0a49dc093c98adc8dba7d5a7
Aime Brave Rugwiro <159177898+Brave-34@users.noreply.github.com>
2025-06-03 21:25:22 +0200

    Update Multiply.js

--- 8 Kyu/Multiply.js
+++ 8 Kyu/Multiply.js
@@ -1,9 +1,7 @@
-<<<<<<< HEAD:8 Kyu/Multiply.js
 function multiply(a, b){
   return a * b
 }
-=======
 function multiply(a, b){
   return a * b
 }
->>>>>>> d1821fa851af0a539adb521bad030b20fbbac4ef:8 Kyu/Sum of Positive/Multiply.js
+
