78cc4c7981a69d5d096177f3220149c6302f4f1e
Aime Brave Rugwiro <159177898+Brave-34@users.noreply.github.com>
2025-06-03 21:32:12 +0200

    Update volumeOfCuboid.js

--- 8 Kyu/volumeOfCuboid.js
+++ 8 Kyu/volumeOfCuboid.js
@@ -1,12 +1,9 @@
-<<<<<<< HEAD:8 Kyu/volumeOfCuboid.js
 class Kata {
   static getVolumeOfCuboid(length, width, height) {
         return length * width * height;
   }
-=======
 class Kata {
   static getVolumeOfCuboid(length, width, height) {
         return length * width * height;
   }
->>>>>>> d1821fa851af0a539adb521bad030b20fbbac4ef:8 Kyu/Sum of Positive/volumeOfCuboid.js
 }
