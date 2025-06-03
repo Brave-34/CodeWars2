8a00b21cfde9d77c3eecc6a50e8646fe05140577
Aime Brave Rugwiro <159177898+Brave-34@users.noreply.github.com>
2025-06-03 21:29:12 +0200

    Update keepHydrated.js

--- 8 Kyu/keepHydrated.js
+++ 8 Kyu/keepHydrated.js
@@ -1,10 +1,7 @@
-<<<<<<< HEAD:8 Kyu/keepHydrated.js
 function litres(time) {
   let amount = time * 0.5;
   return  Math.floor(amount);
-=======
 function litres(time) {
   let amount = time * 0.5;
   return  Math.floor(amount);
->>>>>>> d1821fa851af0a539adb521bad030b20fbbac4ef:8 Kyu/Sum of Positive/keepHydrated.js
 }
