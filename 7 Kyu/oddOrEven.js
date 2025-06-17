ca883f29c3a71f7d538523fd73ce62739ac3a03f
Aime Brave Rugwiro <159177898+Brave-34@users.noreply.github.com>
2025-06-17 22:58:52 +0200

    Solved

--- 7 Kyu/oddOrEven.js
+++ 7 Kyu/oddOrEven.js
@@ -1,10 +1,8 @@
 function oddOrEven(array) {
-   //enter code here
-  let sum = 0;
+let sum = 0;
   for( let i=0; i<array.length; i++){
     sum += array[i];
   }
-  
   if(sum % 2 === 0){
     return "even";
   }
